f7824ed063945f14c2c50901477a0d6d91419fd8
Shahedh0ssain <96790670+Shahedh0ssain@users.noreply.github.com>
2022-03-23 16:28:45 +0600

    Update App.js

--- App.js
+++ App.js
@@ -122,9 +122,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments[1].user}
+                          ${post.comments[1]}
                       </a>
-                      ${post.comments[2].text}
+                      ${post.comments[2]}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
