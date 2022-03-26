0ada931bb0d78f4ad27b6d03552f8835f332ad70
Shahedh0ssain <96790670+Shahedh0ssain@users.noreply.github.com>
2022-03-26 13:10:32 +0600

    Work done

--- script.js
+++ script.js
@@ -40,12 +40,14 @@ const switchTab = (id) => {
         document.getElementById( "liked" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
+        document.getElementById( "question" ).style.display = "none";
 
         displayLikedPosts();
     } else {
         document.getElementById( "reported" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "liked" ).style.display = "none";
+        document.getElementById( "question" ).style.display = "none";
 
         displayReportedPosts();
     }
